084918c2767db6196fd25ea90ba9ccd38fd75893
Jinten <jinten0@gmail.com>
2020-01-05 19:29:12 +0900

    first commit. (previous commit was mistake)

---
+++ code/application.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include"window.h"
+#include"../imgui/imgui.h"
+#include"../imgui/examples/imgui_impl_win32.h"
+#include"../imgui/examples/imgui_impl_dx11.h"
+
+class Application
+{
+public:
+	Application();
+	Application(Window::Desc& desc);
+	~Application();
+
+	void setBackgroundColor(const ImVec4& color)
+	{
+		mBackgroundColor = color;
+	}
+
+	void setShowImGuiDemoWindow(bool enable)
+	{
+		mShowImGuiDemoWindow = enable;
+	}
+
+	void update(Action action);
+
+private:
+	bool mKilled = false;;
+	bool mShowImGuiDemoWindow = false;
+	ImVec4 mBackgroundColor;
+
+	Window mWindow;
+
+private:
+	static void updateMessage(Window::MessageInfo* info);
+};
